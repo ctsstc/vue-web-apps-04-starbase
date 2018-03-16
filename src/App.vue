b3f4af66d04524806b6bdba8355f632d44e2bfcb
Cody Swartz <ctsstc@gmail.com>
2018-03-15 18:31:46 -0700

    Init, up to lecture 24

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h2>{{title}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      title: 'Generate Your Team'
+    }
+  }
+};
+</script>
