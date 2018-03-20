59f79200ccadbf1c27e9cb992782cbda8b741ccc
Cody Swartz <ctsstc@gmail.com>
2018-03-19 19:01:33 -0700

    4.28 - Fetching from a RESTful API

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h2>{{title}}</h2>
+    <button class="btn btn-primary" @click="fetchCharacter">Test Fetch Characters</button>
   </div>
 </template>
 
@@ -9,7 +10,14 @@ export default {
   name: "app",
   data() {
     return {
-      title: 'Generate Your Team'
+      title: "Generate Your Team"
+    };
+  },
+  methods: {
+    fetchCharacter() {
+      fetch("https://swapi.co/api/people/", {
+        method: "GET"
+      }).then(res => console.log(res));
     }
   }
 };
